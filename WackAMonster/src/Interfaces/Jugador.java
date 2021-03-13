a3540a0b5cb45c02ec75065251655d9d863c670c
[Manuel Fernandez] <[manufdzver@hotmail.com]>
2021-03-13 12:30:44 -0600

    cambios a jugador

--- WackAMonster/src/Interfaces/Jugador.java
+++ WackAMonster/src/Interfaces/Jugador.java
@@ -1,24 +1,22 @@
 package Interfaces;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Jugador implements Serializable {
 
     private String nombre;
-    private int puntaje;
     private int numeroDeJuego;
     private int ronda;
 
 
     public Jugador(){
         this.nombre = null;
-        this.puntaje = 0;
         this.numeroDeJuego = 0;
     }
 
     public Jugador(String nombre) {
         this.nombre = nombre;
-        this.puntaje = 0;
         this.numeroDeJuego = 0;
     }
 
@@ -47,19 +45,24 @@ public class Jugador implements Serializable {
         this.nombre = nombre;
     }
 
-    public int getPuntaje() {
-        return puntaje;
-    }
-
-    public void setPuntaje(int puntaje) {
-        this.puntaje = puntaje;
-    }
 
     @Override
     public String toString() {
         return "Jugador{" +
                 ", nombre='" + nombre + '\'' +
-                ", puntaje=" + puntaje +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Jugador jugador = (Jugador) o;
+        return Objects.equals(nombre, jugador.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, numeroDeJuego, ronda);
     }
 }
